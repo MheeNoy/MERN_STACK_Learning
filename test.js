f7f7a144fc6143cae4f59b1ffb53744705a6d8a0
MheeNoy <sattawut64@outlook.com>
2022-12-07 02:25:40 +0700

    step One

---
+++ test.js
@@ -0,0 +1,12 @@
+import http from 'http';
+import express from 'express';
+const app = express();
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+
+}).listen(8080);
